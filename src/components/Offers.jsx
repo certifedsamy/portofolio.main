1979907ebd501f926555354ecea8fa603ed55f75
unknown <samymoulhim05@gmail.com>
2025-01-21 16:24:05 +0100

    news

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -47,7 +47,7 @@ const Offers = () => {
             alt={o.alt || `Image ${index + 1} `}
             className={`mb-3 sm:mb-0 ${index === 0 ? "rounded-t-[30px]" : ""} 
             ${index === 6 ? "rounded-b-[30px]" : ""} 
-            sm:hover:scale-110 sm:transition-all sm:duration-500 w-full h-10 object-cover border`}
+            sm:hover:scale-110 sm:transition-all sm:duration-500 w-full h-10 object-cover`}
           />
           <span className="absolute p-5 text-white text-3xl sm:text-xl sm:py-8 sm:w-[80%] w-auto rounded-xl sm:px-8 sm:bottom-0 bg-black bg-opacity-5 sm:mb-0 backdrop-blur-md sm:transform sm:translate-y-full sm:transition-transform sm:duration-300 sm:group-hover:-translate-y-5">
             {o.domain}{" "}

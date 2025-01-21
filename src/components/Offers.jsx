1515db55d0f345390d84dcc54a556608e96f93d8
unknown <samymoulhim05@gmail.com>
2025-01-21 16:22:29 +0100

    news

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -40,14 +40,14 @@ const Offers = () => {
       className=" px-10 sm:px-48 mt-32 flex flex-col sm:grid sm:gap-12 sm:grid-cols-4 pb-10 font-poppins"
     >
       {offers.map((o, index) => (
-        <div className="offer-item cursor-pointer  sm:rounded-3xl overflow-hidden group">
+        <div className="offer-item cursor-pointer sm:rounded-3xl overflow-hidden group">
           <img
             key={index}
             src={o.img}
             alt={o.alt || `Image ${index + 1} `}
             className={`mb-3 sm:mb-0 ${index === 0 ? "rounded-t-[30px]" : ""} 
             ${index === 6 ? "rounded-b-[30px]" : ""} 
-            sm:hover:scale-110 sm:transition-all sm:duration-500 w-full h-auto object-cover`}
+            sm:hover:scale-110 sm:transition-all sm:duration-500 w-full h-10 object-cover border`}
           />
           <span className="absolute p-5 text-white text-3xl sm:text-xl sm:py-8 sm:w-[80%] w-auto rounded-xl sm:px-8 sm:bottom-0 bg-black bg-opacity-5 sm:mb-0 backdrop-blur-md sm:transform sm:translate-y-full sm:transition-transform sm:duration-300 sm:group-hover:-translate-y-5">
             {o.domain}{" "}
